170b774cd27f06cd699b7f51cc978ce6c9553327
Hiron Das <hcdas.09@gmail.com>
2025-01-11 23:02:41 +0600

    done

--- src/services/__test__/services.test.ts
+++ src/services/__test__/services.test.ts
@@ -1,4 +1,4 @@
-import{it, expect, describe, jest} from "@jest/globals";
+import { it, expect, describe, jest } from "@jest/globals";
 import axios from "axios";
 import { User } from "../../models/user";
 import { UserServices } from "../user-services";
@@ -9,9 +9,9 @@ const mockedAxios = jest.mocked(axios);
 
 describe("User Services", () => {
     it("should signup a user", async () => {
-       // mockedAxios.post.mockResolvedValue({data: {message: "User signed up"}, status: 201});
-        let user:User = {username: "shupti", password: "1234", phone: "123456789"};
-        let response:Response = await new UserServices(process.env.BASE_URL as string).signup(user);
+        mockedAxios.post.mockResolvedValue({ data: { message: "User signed up" }, status: 201 });
+        let user: User = { username: "shupti", password: "1234", phone: "123456789" };
+        let response: Response = await new UserServices(process.env.BASE_URL as string).signup(user);
 
         expect(response.message).toBe("User signed up");
     });
